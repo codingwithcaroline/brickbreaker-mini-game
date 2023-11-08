eca44736841d84c47cc666ee92a6370055507bf4
Caroline Kirkconnell <68080286+trumpetck@users.noreply.github.com>
2023-11-08 09:05:49 -0600

    Add files via upload

---
+++ YouWinWorld.java
@@ -0,0 +1,10 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class YouWinWorld extends World
+{
+    public YouWinWorld()
+    {    
+        super(569, 400, 1, true); 
+        addObject(new YouWin(), 290, 200);
+    }
+}
